c313b5fcc7d92e2482b90f8b6a99a289fd821b42
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-07-10 23:29:42 -0700

    fixed fail issues to make pass in Intern.test

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -8,6 +8,7 @@ test("setting github via constructor arguments", () => {
 });
 // create test for getting getRole()
 test("getting role via getRole()", () => {
+  const school = "schoolName";
   const employee = new Intern("someName", 1234, "random@email.com", school);
   expect(employee.getRole()).toBe("Intern");
 });
